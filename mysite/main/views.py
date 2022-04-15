0a031e409901f89b1b1ddf110a4e4e29881e5eab
Viet281101 <nva281101@gmail.com>
2022-04-15 16:42:30 +0200

    exercices

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,5 +7,10 @@ from .models import ToDoList, Item
 
 def index(response, id):
 	ls = ToDoList.objects.get(id = id)
-	return HttpResponse("<h1> %s </h1>" % ls.name)
+	# item = ls.item_set.get(id = 1)
+	# return HttpResponse("<h1> %s </h1><br></br><p>%s</p>" % (ls.name, str(item.text)))
+	return render(response, "main/base.html", {"name": ls.name})
 
+
+def home(response):
+	return render(response, "main/home.html", {})
